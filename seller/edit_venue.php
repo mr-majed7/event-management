f9bc89f4ccad774c578c1407be61799cd9c5310b
Majed <majedulislam7789@gmail.com>
2023-12-06 20:38:11 +0600

    minor_refactoring

--- seller/edit_venue.php
+++ seller/edit_venue.php
@@ -9,21 +9,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $price = $_POST['price'];
     $capacity = $_POST['capacity'];
 
-    // Update query for the Venue associated with the manager_id
     $update_query = "UPDATE Venue 
                      SET name = '$venue_name', area = '$area', city = '$city', price = '$price', capacity = '$capacity' 
                      WHERE id IN (SELECT ven_id FROM Manager WHERE id = '$manager_id')";
 
     if ($conn->query($update_query) === TRUE) {
-        // Successful update, redirect to seller_home.php with the manager_id
         header("Location: seller_home.php?manager_id=$manager_id");
         exit();
     } else {
-        // Error in update query
         echo "Error updating venue: " . $conn->error;
     }
 } else {
-    // Redirect if accessed without POST method
     header("Location: ../index.html");
     exit();
 }
