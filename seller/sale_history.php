fdc4d5097159c76e7ddbdb0a12b516106fa2ef42
Toufik333 <75950762+Toufik333@users.noreply.github.com>
2023-12-09 16:04:46 +0600

    with name

--- seller/sale_history.php
+++ seller/sale_history.php
@@ -5,8 +5,14 @@ include_once("../db_connection.php");
 // Initialize variables
 $bookings = array();
 
-// SQL query to fetch all bookings
-$query = "SELECT * FROM Bookings";
+// SQL query to fetch all bookings with joined data
+$query = "SELECT b.*, v.name AS venue_name, c.first_name, c.last_name AS customer_name, e.name AS event_name, s.name AS supplier_name
+FROM Bookings b
+INNER JOIN Venue v ON b.ven_id = v.id
+INNER JOIN Customer c ON b.cust_id = c.id
+INNER JOIN Events e ON b.ev_id = e.id
+INNER JOIN Supplier s ON b.supp_id = s.id";
+
 $result = $conn->query($query);
 
 // Check if any bookings found
@@ -32,34 +38,38 @@ $conn->close();
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-  <div class="container
- 
-mt-5">
-    <div
- 
-class="card">
-      <div
- 
-class="card-header">
+  <div class="container mt-5">
+    <div class="card">
+      <div class="card-header">
         <h3>Bookings List</h3>
       </div>
       <div class="card-body">
         <table class="table table-striped table-bordered">
           <thead>
             <tr>
-              <th>Ven_ID</th>
-              <th>Ev_ID</th>
-              <th>Cust_ID</th>
-              <th>Supp_ID</th>
+              
+              <th>Venue ID</th>
+              <th>Venue Name</th>
+              <th>Customer ID</th>
+              <th>Customer Name</th>
+              <th>Event ID</th>
+              <th>Event Name</th>
+              <th>Supplier ID</th>
+              <th>Supplier Name</th>
             </tr>
           </thead>
           <tbody>
             <?php foreach ($bookings as $booking) : ?>
               <tr>
+                
                 <td><?php echo $booking['ven_id']; ?></td>
-                <td><?php echo $booking['ev_id']; ?></td>
+                <td><?php echo $booking['venue_name']; ?></td>
                 <td><?php echo $booking['cust_id']; ?></td>
+                <td><?php echo $booking['customer_name']; ?></td>
+                <td><?php echo $booking['ev_id']; ?></td>
+                <td><?php echo $booking['event_name']; ?></td>
                 <td><?php echo $booking['supp_id']; ?></td>
+                <td><?php echo $booking['supplier_name']; ?></td>
               </tr>
             <?php endforeach; ?>
           </tbody>
